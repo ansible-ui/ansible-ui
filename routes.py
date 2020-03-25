2b370dca518ee08a24f325529ccbb1a18ca258da
0xe590b4 <0xe590b4@gmail.com>
2020-03-25 16:52:19 +0800

    add package

--- routes.py
+++ routes.py
@@ -12,51 +12,47 @@ from apps.soft.view import *
 from apps.cmdb.view import *
 from apps.web.view import *
 from apps.game.view import *
-from apps.analysis.view import *
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def setup_routes(app):
-    app.router.add_get('', handler_win10),
+    app.router.add_get('', handler_win10)
 
-    app.router.add_get('/cloud', handler_cloud),
-    app.router.add_get('/cloud/terraform_list', handler_terraform_list),
-    app.router.add_post('/cloud/terraform_read', handler_terraform_read),
-    app.router.add_post('/cloud/terraform_write', handler_terraform_write),
+    app.router.add_get('/cloud', handler_cloud)
+    app.router.add_get('/cloud/terraform_list', handler_terraform_list)
+    app.router.add_post('/cloud/terraform_read', handler_terraform_read)
+    app.router.add_post('/cloud/terraform_write', handler_terraform_write)
     app.router.add_get('/cloud/ws', ws_terraform_run)
 
-    app.router.add_get('/soft', handler_soft),
-    app.router.add_get('/soft/ansible_list', handler_ansible_list),
-    app.router.add_post('/soft/ansible_read', handler_ansible_read),
-    app.router.add_post('/soft/ansible_write', handler_ansible_write),
+    app.router.add_get('/soft', handler_soft)
+    app.router.add_get('/soft/ansible_list', handler_ansible_list)
+    app.router.add_post('/soft/ansible_read', handler_ansible_read)
+    app.router.add_post('/soft/ansible_write', handler_ansible_write)
     app.router.add_get('/soft/workflow', handler_soft_workflow)
     app.router.add_post('/soft/workflow/roles', handler_soft_workflow_roles)
     app.router.add_get('/soft/ws', ws_ansible_run)
 
-    app.router.add_get('/game', handler_game),
-    app.router.add_get('/game/ansible_list', handler_game_ansible_list),
-    app.router.add_post('/game/ansible_read', handler_game_ansible_read),
-    app.router.add_post('/game/ansible_write', handler_game_ansible_write),
+    app.router.add_get('/game', handler_game)
+    app.router.add_get('/game/ansible_list', handler_game_ansible_list)
+    app.router.add_post('/game/ansible_read', handler_game_ansible_read)
+    app.router.add_post('/game/ansible_write', handler_game_ansible_write)
     app.router.add_get('/game/workflow', handler_game_workflow)
     app.router.add_post('/game/workflow/roles', handler_game_workflow_roles)
     app.router.add_get('/game/ws', handler_game_ws_ansible_run)
 
-    app.router.add_get('/cmdb', handler_servers_list),
-    app.router.add_get('/cmdb/database_list', handler_database_list),
-    app.router.add_get('/cmdb/database_list_data', handler_database_list_data),
-    app.router.add_get('/cmdb/servers_list', handler_servers_list),
-    app.router.add_get('/cmdb/servers_list_data', handler_servers_list_data),
-    app.router.add_get('/cmdb/crontab_task', handler_crontab_task),
+    app.router.add_get('/cmdb', handler_servers_list)
+    app.router.add_get('/cmdb/database_list', handler_database_list)
+    app.router.add_get('/cmdb/database_list_data', handler_database_list_data)
+    app.router.add_get('/cmdb/servers_list', handler_servers_list)
+    app.router.add_get('/cmdb/servers_list_data', handler_servers_list_data)
+    app.router.add_get('/cmdb/crontab_task', handler_crontab_task)
 
+    app.router.add_get('/web', handler_web)
+    app.router.add_get('/cmdb/servers_list', handler_game)
 
-    app.router.add_get('/web', handler_web),
-    app.router.add_get('/analysis', handler_analysis),
-
-
-    app.router.add_get('/cmdb/servers_list', handler_game),
-    app.router.add_get('/game/workflow', handler_game_workflow),
 
     # app.router.add_get('/session', handler_session),
     # app.router.add_get('/', index)
